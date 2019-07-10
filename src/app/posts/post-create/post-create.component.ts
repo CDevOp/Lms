694bce78e8604c4db06c4cb1a0340b03b5c5c55e
ColinMCH <chumakc@pm.me>
2019-07-10 11:25:46 -0700

    Added postsService using  rxjs, subscriptions and observables

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,27 +1,23 @@
-import { Component, EventEmitter, Output } from "@angular/core";
-
-import { Post } from "../post.model";
-import { NgForm } from "@angular/forms";
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-post-create",
-  templateUrl: "./post-create.component.html",
-  styleUrls: ["./post-create.component.css"]
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredContent = "";
-  enteredTitle = "";
-  @Output() postCreated = new EventEmitter<Post>();
+  enteredContent = '';
+  enteredTitle = '';
+
+  constructor(public postsService: PostsService) { }
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content
-    };
 
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 }
